3c1f504fd0552c87ef21b1f7ecc5291258061219
Georgi Lyubenov <godzbanebane@gmail.com>
2022-02-23 17:07:33 +0200

    Add cheat names to the live file

--- src/Printf.hs
+++ src/Printf.hs
@@ -1,1 +1,12 @@
 module Printf where
+
+-- FormatType - *Ty
+-- Format - :%, :>, infixr
+-- printfFormat
+
+-- FormatArg - *Arg
+-- Parse
+-- SpanFormat
+-- ToFormatArgs
+-- FormatArgsToFormat
+-- printf
